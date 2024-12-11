2cf2acd675aadbc3b7b6a500fef29133e057ac5a
atabay.arniyazov <atabay.arniyazov@indev.by>
2024-12-11 17:40:01 +0300

    - add new 'DrawingLinesComponent' example;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ThreejsVisualComponent } from './three-js-visual/three-js-visual.component';
+import { DrawingLinesComponent } from './drawing-lines/drawing-lines.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'example_1', component: ThreejsVisualComponent },
+  { path: 'example_2', component: DrawingLinesComponent },
 ];
